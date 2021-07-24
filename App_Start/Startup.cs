4c728cc018502f63b2a1a1c0ece01c927760d453
Jonathan Bek <jonathan_bek@lexia-analytics.com>
2021-07-24 17:22:15 +0800

    Did MS Login (compulsory, fine tune next time)

---
+++ App_Start/Startup.cs
@@ -0,0 +1,49 @@
+﻿using System.Configuration;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.Owin;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OpenIdConnect;
+using Owin;
+
+[assembly: OwinStartup(typeof(Jikandesu.App_Start.Startup))]
+
+namespace Jikandesu.App_Start
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+            app.SetDefaultSignInAsAuthenticationType(CookieAuthenticationDefaults.AuthenticationType);
+            app.UseCookieAuthentication(new CookieAuthenticationOptions());
+            app.UseOpenIdConnectAuthentication(
+                new OpenIdConnectAuthenticationOptions
+                {
+                    // Sets the client ID, authority, and redirect URI as obtained from Web.config
+                    ClientId = ConfigurationManager.AppSettings.Get("ClientId"),
+                    Authority = ConfigurationManager.AppSettings.Get("Authority"),
+                    RedirectUri = ConfigurationManager.AppSettings.Get("redirectUri"),
+                    // PostLogoutRedirectUri is the page that users will be redirected to after sign-out. In this case, it's using the home page
+                    PostLogoutRedirectUri = ConfigurationManager.AppSettings.Get("redirectUri"),
+                    Scope = OpenIdConnectScope.OpenIdProfile,
+                    // ResponseType is set to request the code id_token, which contains basic information about the signed-in user
+                    ResponseType = OpenIdConnectResponseType.CodeIdToken,
+                    // ValidateIssuer set to false to allow personal and work accounts from any organization to sign in to your application
+                    // To only allow users from a single organization, set ValidateIssuer to true and the 'tenant' setting in Web.config to the tenant name
+                    // To allow users from only a list of specific organizations, set ValidateIssuer to true and use the ValidIssuers parameter
+                    TokenValidationParameters = new TokenValidationParameters()
+                    {
+                        ValidateIssuer = false // Simplification (see note below)
+                    },
+                    // OpenIdConnectAuthenticationNotifications configures OWIN to send notification of failed authentications to the OnAuthenticationFailed method
+                    Notifications = new OpenIdConnectAuthenticationNotifications
+                    {
+                        //AuthenticationFailed = OnAuthenticationFailed
+                    }
+                }
+            );
+        }
+    }
+}
